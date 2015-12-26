e50ee9a83e33fc8a1940378cfe1ca692ba64d803
alantitor <belosianau@gmail.com>
2015-12-26 23:24:51 +0800

    setup program.

---
+++ src/eyny_comic_parser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def process(argv):
+	# get variable from options.
+
+	# get source list.
+
+	# filter data.
+
+	# save data.
+
+	pass
+
+if __name__ == "__main__":
+	reload(sys)
+	sys.setdefaultencoding('utf8')
+	process(sys.argv)
