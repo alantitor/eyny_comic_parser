ccc6a9b6dc953c00af5d2b1fa3580b2828d47792
alantitor <belosianau@gmail.com>
2016-01-01 23:43:14 +0800

    put system variables here.

---
+++ src/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# define comic image width
+IMAGE_WIDTH = 1000
+# define comic image heigh
+IMAGE_HEIGH = 1000
+# define comic title
+GARBAGE_TITLE_STRING = ''
